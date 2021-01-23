a8467499a14f1fe349c34240f334078bf714cdf6
sp3ber <sp3ber@gmail.com>
2021-01-23 19:26:39 +0000

    Deploying to gh-pages from  @ 4d4c80d247cd270b4151a6e5c2e971aff75c892c 🚀

--- src/__tests__/main.test.ts
+++ src/__tests__/main.test.ts
@@ -23,11 +23,21 @@ const bootstrapHtml = () => {
 	const html = $(".container").toString();
 	document.body.innerHTML = html;
 	run();
+	execCommand.mockReset(); // ресетим, так как есть служебные вызовы в начале
 	return {
 		editorArea: screen.getByTestId("edit-area"),
+		h1: screen.getByTestId("head-1"),
+		h2: screen.getByTestId("head-2"),
+		p: screen.getByTestId("paragraph"),
+		b: screen.getByTestId("bold"),
+		i: screen.getByTestId("italic"),
+		execCommand,
+		queryCommandState,
 	};
 };
 
+// Данные базовый набор тестов проходит уже на стадии использования атрибута contentEditable, но может быть полезен
+// при переходе на другую архитектуру (например, на designMode)
 describe("simple wysiwig editor", () => {
 	test("Введенный текст отображается", async () => {
 		const { editorArea } = bootstrapHtml();
@@ -61,22 +71,80 @@ describe("simple wysiwig editor", () => {
 	});
 });
 
+// Данные тесты достаточно сильно завязаны на реализацию (documentExec и класс active), однако позволяют проводить
+// некоторый рефакторинг
 describe("Контролы", () => {
-	test.todo(
-		"При нажатии на жирный вызывается команда и кнопка становится жирной"
-	);
-	test.todo(
-		"При нажатии на заголовок первого уровня вызывается команда и кнопка становится жирной"
-	);
-	test.todo(
-		"При нажатии на заголовок второго уровня вызывается команда и кнопка становится жирной"
-	);
-	test.todo(
-		"При нажатии на параграф вызывается команда и кнопка становится жирной"
-	);
-	test.todo(
-		"При нажатии на курсив вызывается команда и кнопка становится жирной"
-	);
+	test("При нажатии на заголовок первого уровня вызывается команда и кнопка становится жирной", () => {
+		const { editorArea, h1, execCommand } = bootstrapHtml();
+		expect(execCommand).not.toHaveBeenCalled();
+		userEvent.type(editorArea, "hello MISHA");
+		userEvent.click(h1);
+		expect(execCommand).toHaveBeenCalledWith("formatBlock", false, "h1");
+	});
+	test("При нажатии на заголовок второго уровня вызывается команда", () => {
+		const { editorArea, h2, execCommand } = bootstrapHtml();
+		expect(execCommand).not.toHaveBeenCalled();
+		userEvent.type(editorArea, "hello MISHA");
+		userEvent.click(h2);
+		expect(execCommand).toHaveBeenCalledWith("formatBlock", false, "h2");
+	});
+	test("При нажатии на параграф вызывается команда", () => {
+		const { editorArea, p, execCommand } = bootstrapHtml();
+		expect(execCommand).not.toHaveBeenCalled();
+		userEvent.type(editorArea, "hello MISHA");
+		userEvent.click(p);
+		expect(execCommand).toHaveBeenCalledWith("formatBlock", false, "p");
+	});
+	test("При нажатии и отжатии на жирный вызываются соответствующие команды и меняется активность кнопки", () => {
+		const { editorArea, b, execCommand } = bootstrapHtml();
+
+		// Проверяем изначальное состояния
+		expect(execCommand).not.toHaveBeenCalled();
+		expect(b).not.toHaveClass("active");
+
+		// Включаем
+		userEvent.type(editorArea, "hello MISHA");
+		userEvent.click(b);
+
+		// Проверяем
+		expect(execCommand).toHaveBeenCalledWith("bold", false, undefined);
+		expect(b).toHaveClass("active");
+
+		// Сбрасываем моки
+		execCommand.mockReset();
+
+		// Выключаем
+		userEvent.click(b);
+
+		// Проверяем
+		expect(execCommand).toHaveBeenCalledWith("bold", false, undefined);
+		expect(b).not.toHaveClass("active");
+	});
+	test("При нажатии и отжатии на курсив вызываются соответствующие команды и меняется активность кнопки", () => {
+		const { editorArea, i, execCommand } = bootstrapHtml();
+
+		// Проверяем изначальное состояние
+		expect(execCommand).not.toHaveBeenCalled();
+		expect(i).not.toHaveClass("active");
+
+		// Включаем
+		userEvent.type(editorArea, "hello MISHA");
+		userEvent.click(i);
+
+		// Проверяем
+		expect(execCommand).toHaveBeenCalledWith("italic", false, undefined);
+		expect(i).toHaveClass("active");
+
+		// Сбрасываем моки
+		execCommand.mockReset();
+
+		// Отключаем
+		userEvent.click(i);
+
+		// Проверяем
+		expect(execCommand).toHaveBeenCalledWith("italic", false, undefined);
+		expect(i).not.toHaveClass("active");
+	});
 });
 
 describe("Копирование и вырезание", () => {
